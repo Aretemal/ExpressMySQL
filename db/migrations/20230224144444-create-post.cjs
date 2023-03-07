96723279a0996ac1d56454b5389fc913c14dd6b4
Aretemal <arproggamoi@gmail.com>
2023-03-07 22:15:49 +0300

    SN-API-3 improvement: update migrations

--- db/migrations/20230224144444-create-post.cjs
+++ db/migrations/20230224144444-create-post.cjs
@@ -1,5 +1,5 @@
 /** @type {import('sequelize-cli').Migration} */
-export const p = {
+module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Posts', {
       id: {
