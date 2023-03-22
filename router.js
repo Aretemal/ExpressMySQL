e4a2d6d56c1233df1fc39a18c609d1d862a7e7e1
Aretemal <arproggamoi@gmail.com>
2023-03-22 18:00:52 +0300

    SN-API-37 improvement: delete tryCatch file

--- router.js
+++ router.js
@@ -6,36 +6,34 @@ import ProfileController from './src/controllers/ProfileController.js';
 import UserController from './src/controllers/UserController.js';
 import AuthController from './src/controllers/AuthController.js';
 import { tokenMiddleware } from './src/middlewaree/tokenMiddleware.js';
-import { tryCatch } from './src/utils/tryCatch.js';
 
 const router = new Router();
-
 router.post('/registration', [
   check('userName', 'Имя пользовател не может быть пустым').notEmpty(),
   check('password', 'Password не может быть короче 4 символов').isLength({ min: 4 }),
-], tryCatch(AuthController.registration));
-router.post('/login', tryCatch(AuthController.login));
+], AuthController.registration);
+router.post('/login', AuthController.login);
 
 router.use(tokenMiddleware);
 
 // Users
-router.get('/user/:id', tryCatch(UserController.getOne));
-router.get('/users/find', tryCatch(UserController.getAllUsers));
-router.get('profile/status/:id', tryCatch(UserController.getStatus));
+router.get('/user/:id', UserController.getOne);
+router.get('/users/find', UserController.getAllUsers);
+router.get('profile/status/:id', UserController.getStatus);
 
 // Post
-router.post('/profile/posts', tryCatch(PostController.create));
-router.get('/profile/posts', tryCatch(PostController.getAll));
-router.get('/profile/posts/:id', tryCatch(PostController.getOne));
-router.put('/profile/posts/:id', tryCatch(PostController.update));
-router.delete('/profile/posts/:id', tryCatch(PostController.delete));
+router.post('/profile/posts', PostController.create);
+router.get('/profile/posts', PostController.getAll);
+router.get('/profile/posts/:id', PostController.getOne);
+router.put('/profile/posts/:id', PostController.update);
+router.delete('/profile/posts/:id', PostController.delete);
 
-router.get('/profile/user', tryCatch(ProfileController.getInfoAuthorizedUser));
-router.put('/profile/status', tryCatch(ProfileController.updateStatus));
+router.get('/profile/user', ProfileController.getInfoAuthorizedUser);
+router.put('/profile/status', ProfileController.updateStatus);
 
 // Follow
-router.post('/follow', tryCatch(FollowController.follow));
-router.put('/approve', tryCatch(FollowController.approve));
-router.put('/unfollow', tryCatch(FollowController.unfollow));
+router.post('/follow', FollowController.follow);
+router.put('/approve', FollowCosntroller.approve);
+router.put('/unfollow', FollowController.unfollow);
 
 export default router;
