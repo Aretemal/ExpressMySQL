8bd90a34efb8ab5de8c13a84b34971aea5be799f
Aretemal <arproggamoi@gmail.com>
2023-04-12 17:42:18 +0300

    SN-API-44 feat: add new method for Follow and Dialog services and controllers

--- src/controllers/FollowController.js
+++ src/controllers/FollowController.js
@@ -1,4 +1,6 @@
 import { validationResult } from 'express-validator';
+import CollectionSerializer from '../serializers/CollectionSerializer.js';
+import UserSerializer from '../serializers/UserSerializer.js';
 import FollowService from '../services/FollowService.js';
 import FollowSerializer from '../serializers/FollowSerializer.js';
 
@@ -33,6 +35,24 @@ class FollowController {
     const { id } = req.body;
     const follow = await FollowService.unfollow(req.user.id, id);
     req.serializer = new FollowSerializer(follow, req);
+    next();
+  }
+
+  async getFriends(req, res, next) {
+    const friends = await FollowService.getFriends(req.user.id);
+    req.serializer = new CollectionSerializer(friends, UserSerializer, req);
+    next();
+  }
+
+  async getSubscriptions(req, res, next) {
+    const subscriptions = await FollowService.getSubscriptions(req.user.id);
+    req.serializer = new CollectionSerializer(subscriptions, UserSerializer, req);
+    next();
+  }
+
+  async getSubscribers(req, res, next) {
+    const subscribers = await FollowService.getSubscribers(req.user.id);
+    req.serializer = new CollectionSerializer(subscribers, UserSerializer, req);
     next();
   }
 }
